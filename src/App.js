c22ee37bbd1553ec6e64ca9ba9633cce879fcffc
Ryugwanwoo <locotalk@naver.com>
2020-11-18 21:39:34 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let's develop management system!</h2>
-        
-    </div>
-  );
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/1',
+    'name' : '류관우',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '홍길동',
+    'birthday' : '960522',
+    'gender' : '남자',
+    'job' : '프로그래머'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '이순신',
+    'birthday' : '966822',
+    'gender' : '남자',
+    'job' : '디자이너'
+  }
+]
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return( 
+            <Customer
+              key={c.id}
+              id={c.id}
+              iamge={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              job={c.job}
+            />
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
